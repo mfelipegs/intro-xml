9d7fd0e7612128fe4437b77428be5de0faf19bb3
mfelipegs <matheusg.dasilva7@gmail.com>
2022-08-31 22:17:25 -0300

    Finished

--- js/funcoes.js
+++ js/funcoes.js
@@ -1,7 +1,29 @@
+/* Lê o XML */  
 xmlhttp = new XMLHttpRequest();
 xmlhttp.open("GET", "xml/db_conteudo.xml", false);
 xmlhttp.send();
-//informo q arquivo q to trabalhando e do tipo xml
 xmlDoc = xmlhttp.responseXML;
+let x = xmlDoc.getElementsByTagName("postagem");
 
-let x = xmlDoc.getElementsByTagName("postagem")
+/* Exibe o conteúdo */
+function funcaoConteudo(){
+    n = x.length-1;
+    for (var i = n; i >= 0; i--){
+        var destaque = (i == n)?'#c0c0c0':'#ffffff';
+        document.write("<tr bgcolor='"+ destaque +"'>"+
+                    "<td><a href='postagem.html?id=" + i + "'>"+ x[i].getAttribute("codigo") + "</a></td>" +
+                    "<td><img src='imgs/" + x[i].getElementsByTagName("imagem_1")[0].childNodes[0].nodeValue + "' width='150'></td>" +
+                    "<td>" + x[i].getElementsByTagName("titulo")[0].childNodes[0].nodeValue + "</td>" +          
+                    "<td>" + x[i].getElementsByTagName("corpo")[0].childNodes[0].nodeValue.substr(0,250) + "</td></tr>");}
+}
+
+/* Exibe a postagem */  
+function funcaoPostagem(){
+    let url = new URL(window.location.href);
+    let search_params = url.searchParams; 
+    i = search_params.get('id');    
+        document.write("<tr bgcolor='#ffffff'><td>"+ x[i].getAttribute("codigo") + "</td>" +
+                       "<td><img src='imgs/" + x[i].getElementsByTagName("imagem_1")[0].childNodes[0].nodeValue + "' width='150'></td>" +
+                       "<td>" + x[i].getElementsByTagName("titulo")[0].childNodes[0].nodeValue + "</td>" +
+                       "<td>" + x[i].getElementsByTagName("corpo")[0].childNodes[0].nodeValue + "</td></tr>");      
+}
